c832d84277d13341f1839116f0821e3aa24e00b1
Ali Raza <infoaliraza22@gmail.com>
2024-10-25 12:38:08 +0500

    Create Mongo Db Connection successfully

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("File is working");
+const mongoose = require("mongoose");
+
+
+
+// Connect to MongoDB
+async function connectDb() {
+    try {
+        await mongoose.connect("mongodb://localhost:27017/SeebizDb", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log("Connection to MongoDB Successful!");
+    } catch (err) {
+        console.error("Error Connecting with MongoDB:", err);
+    }
+}
+
+connectDb();
+
+
+
+
+
