badf007179c324a7d91f584337e78a67ea26f455
Ali Raza <infoaliraza22@gmail.com>
2024-10-25 13:17:35 +0500

    Creating connection , scehma and new user and find user

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
+const { type } = require("os");
 
 
 
 // Connect to MongoDB
 async function connectDb() {
     try {
-        await mongoose.connect("mongodb://localhost:27017/SeebizDb", {
+        await mongoose.connect("mongodb://localhost:27017/SignUpDb", {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
@@ -17,6 +18,60 @@ async function connectDb() {
 }
 
 connectDb();
+
+
+// Creating User Schema 
+
+const userSchema = new mongoose.Schema({
+    name:{type:String , required : true},
+    email:{type:String , required : true , unique : true},
+    password : {type:String , required : true}
+});
+
+// Creating User Model
+
+const userData = mongoose.model('userData' , userSchema);
+
+// save a user 
+ 
+// const newUser = new userData({
+//     name : "Ali Raza Afzal",
+//     email : "ali@gmail.com",
+//     password : "123456"
+// });
+
+// newUser.save()
+// .then(result => console.log('user created ' , result))
+// .catch(error => console.log('user not created' , error));
+
+// using Create method
+
+// userData.create({
+//     name:"Abdullah Afzal",
+//     email:"abdullahafzal@seebiz.com",
+//     password:"123456"
+// }).
+// then(result => console.log('user created ' , result))
+// .catch(result => console.log('user not created successfully' , result));
+
+
+// show users using find() method
+
+// userData.find()
+// .then(result => console.log("All Users" , result))
+// .catch(error => console.log("Error in fetching user" , error));
+
+// show user by findOne() method
+userData.findOne({email:"ali@gmail.com"})
+.then(user => console.log("User" , user))
+.catch(user => console.log("not user" , user));
+
+
+
+
+
+
+
 
 
 
