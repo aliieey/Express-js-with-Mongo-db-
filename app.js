4f3b9de517710a4a40c7ee531554a8f23b552f89
Ali Raza <infoaliraza22@gmail.com>
2024-10-25 13:42:59 +0500

    Complete CRUD Operations in Mongo Db

--- app.js
+++ app.js
@@ -46,13 +46,13 @@ const userData = mongoose.model('userData' , userSchema);
 
 // using Create method
 
-// userData.create({
-//     name:"Abdullah Afzal",
-//     email:"abdullahafzal@seebiz.com",
-//     password:"123456"
-// }).
-// then(result => console.log('user created ' , result))
-// .catch(result => console.log('user not created successfully' , result));
+userData.create({
+    name:"Bilal Zafar",
+    email:"bz@gmail.com",
+    password:"123456"
+}).
+then(result => console.log('user created ' , result))
+.catch(result => console.log('user not created successfully' , result));
 
 
 // show users using find() method
@@ -62,9 +62,56 @@ const userData = mongoose.model('userData' , userSchema);
 // .catch(error => console.log("Error in fetching user" , error));
 
 // show user by findOne() method
-userData.findOne({email:"ali@gmail.com"})
-.then(user => console.log("User" , user))
-.catch(user => console.log("not user" , user));
+
+// userData.findOne({email:"ali@gmail.com"})
+// .then(user => console.log("User" , user))
+// .catch(user => console.log("not user" , user));
+
+
+// show user by findById() method
+
+userData.findById("671b50864ef3ec81d7242ffb")
+.then(userId => console.log('user by id' , userId))
+.catch(userId => console.log('user by id' , userId));
+
+
+// update user by updateOne() method 
+
+// userData.updateOne({email:"ali@gmail.com"} , {name:"Ali Afzal"})
+// .then(updateName => console.log('UpdateName' , updateName))
+// .catch(error => console.log('Error' , error));
+
+
+// update user by findByIdAndUpdate() method
+
+userData.findByIdAndUpdate("671b50864ef3ec81d7242ffb" , {name : "Abdullah Afzal"})
+.then(user => console.log("user updated" , user))
+.catch(error => console.log("user not updated"));
+
+
+
+// delete user by findByIdAndDelete() method
+
+userData.findByIdAndDelete("671b5820a3d1b6fa9311d3ef")
+.then(user =>console.log("user deleted" , user))
+.catch(error => console.log("error deleting the user" , error));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
